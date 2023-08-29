ce767aac98427eed423fd8839c506ef4ddeea601
dfldylan <dfldylan@qq.com>
2023-08-29 10:55:06 +0800

    feat(container): autostop container over 7 days

--- api/docker/snapshot.go
+++ api/docker/snapshot.go
@@ -166,7 +166,6 @@ func snapshotContainers(snapshot *portainer.DockerSnapshot, cli *client.Client) 
 		} else if container.State == "running" {
 			runningContainers++
 
-			// snapshot GPUs
 			response, err := cli.ContainerInspect(context.Background(), container.ID)
 			if err != nil {
 				// Inspect a container will fail when the container runs on a different
@@ -183,6 +182,7 @@ func snapshotContainers(snapshot *portainer.DockerSnapshot, cli *client.Client) 
 					log.Debug().Str("container", container.ID).Err(err).Msg("unable to inspect container in other Swarm nodes")
 				}
 			} else {
+				// snapshot GPUs
 				var gpuOptions *_container.DeviceRequest = nil
 				for _, deviceRequest := range response.HostConfig.Resources.DeviceRequests {
 					deviceRequest := deviceRequest
@@ -197,6 +197,15 @@ func snapshotContainers(snapshot *portainer.DockerSnapshot, cli *client.Client) 
 					}
 					for _, id := range gpuOptions.DeviceIDs {
 						gpuUseSet[id] = struct{}{}
+					}
+				}
+
+				// disposal of expired containers
+				if response.State.Running {
+					var startedAt string = response.State.StartedAt
+					t, _ := time.Parse(time.RFC3339Nano, startedAt)
+					if time.Since(t).Hours() > 24*7 && !strings.Contains(response.Name, "portainer") && !strings.Contains(response.Name, "ai3d") {
+						cli.ContainerStop(context.Background(), container.ID, _container.StopOptions{})
 					}
 				}
 			}
